9b5e5000fa3bc6c45599ef48035c20444b308656
aumitleon <aumitleon1@gmail.com>
2019-12-26 03:13:03 -0500

    fix(components): use functional style

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import FileUpload from './components/FileUpload';
+import logo from '../logo.svg';
 
-function App() {
+function FileUpload() {
   return (
     <div className="App">
       <header className="App-header">
@@ -17,4 +15,4 @@ function App() {
   );
 }
 
-export default App;
+export default FileUpload;
