ac38af6c36bc617c2a74ffd568fb729a6a1ebc4d
aumitleon <aumitleon1@gmail.com>
2019-12-30 02:44:09 -0500

    perf(doc-upload-api): adjust rate limit to 50 files/hr

--- server/server.js
+++ server/server.js
@@ -44,8 +44,8 @@ const upload = multer({
 // see https://expressjs.com/en/guide/behind-proxies.html
 // app.set('trust proxy', 1);
 const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // 100 requests per IP within 15 minute window
+  windowMs: 60 * 60 * 1000, // 15 minutes
+  max: 50 // 100 requests per IP within 15 minute window
 });
 
 // logging config
